61eb692edb9ffd873f903a82e2d8add34955709f
Hariadi Hinta <diperakui@yahoo.com>
2013-08-04 22:10:48 +0800

    Clean html files on _gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,7 +80,7 @@ module.exports = function(grunt) {
       android: {
         files: [
           {expand: true, cwd: '<%= pretty %>',   src: ['*.html'], dest: '<%= dest %>'},
-          {expand: true, cwd: '<%= dest %>',   src: ['**'], dest: '<%= ghpages %>'},
+          {expand: true, cwd: '<%= dest %>',   src: ['*.html'], dest: '<%= ghpages %>'},
           {expand: true, cwd: 'src/assets',   src: ['**'], dest: '<%= ghpages %>/assets'},
         ]
       }
@@ -88,8 +88,9 @@ module.exports = function(grunt) {
 
     // remove files from previous build.
     clean: {
+      ghpages: ['<%= ghpages %>/*.html'],
       dist: ['<%= dest %>'],
-      pretty: ['<%= pretty %>']
+      //pretty: ['<%= pretty %>']
     }
   });
 
@@ -99,7 +100,7 @@ module.exports = function(grunt) {
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   // Default task to be run.
-  grunt.registerTask('default', ['clean', 'less', 'assemble', 'prettify', 'copy', 'clean']);
+  grunt.registerTask('default', ['clean', 'less', 'assemble', 'prettify', 'copy']);
 };
 
 
