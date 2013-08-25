baab7c360cb75a83e8ea9b6f8121d52c330d968f
Hariadi Hinta <diperakui@yahoo.com>
2013-08-25 22:02:12 +0800

    Tweak helper.

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -1,24 +1,14 @@
-// Base on Assemble withSort helper
-
 (function() {
 
-  reversed: reversed = function(array, options) {
-      var getDescendantProp, item, result, _i, _len;
-      getDescendantProp = function(obj, desc) {
-        var arr;
-        arr = desc.split(".");
-        while (arr.length && (obj = obj[arr.shift()])) {
-          continue;
-        }
-        return obj;
-      };
+  reversed: reversed = function(array, count, options) {
+      var item, result, _i, _len;
+
       result = '';
       array = array.reverse();
       for (_i = 0, _len = array.length; _i < _len; _i++) {
         item = array[_i];
         result += options.fn(item);
       }
-
       return result;
     };
 
