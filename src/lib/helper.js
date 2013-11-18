04851c3cf9e3fd2fc2f37a03a03144ab46c7bf3f
Hariadi Hinta <diperakui@yahoo.com>
2013-11-18 11:43:07 +0800

    fix(helper): one file for all helpers
    Moving slugify to helpers.js

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -1,19 +1,54 @@
-(function() {
 
-  reversed: reversed = function(array, count, options) {
-      var item, result, _i, _len;
+'use strict';
+var helpers = {
+  /**
+   * {{reversed}}
+   * Returns the derived reversed path from A to B.
+   * @param  {Array}  array
+   * @param  {String}  count
+   * @param  {Object} options
+   * @return {[String]}   [result]
+   * @example:
+   *   {{reversed string}}
+   */
+  reversed: function(array, count, options) {
+    var item, result, _i, _len;
 
-      result = '';
-      array = array.reverse();
-      for (_i = 0, _len = array.length; _i < _len; _i++) {
-        item = array[_i];
-        result += options.fn(item);
-      }
-      return result;
-    };
+    result = '';
+    array = array.reverse();
+    for (_i = 0, _len = array.length; _i < _len; _i++) {
+      item = array[_i];
+      result += options.fn(item);
+    }
+    return result;
+  },
 
-  module.exports.register = function(Handlebars, options) {
-    Handlebars.registerHelper('reversed', reversed);
-  };
+  slugify: function(input) {
+    if (!input || typeof input !== 'string') return input;
 
-}).call(this);
+    input = input.replace(/^\s+|\s+$/g, '').toLowerCase();
+
+    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    var to = "aaaaeeeeiiiioooouuuunc------";
+
+    for (var i=0, l=from.length ; i<l ; i++) {
+      input = input.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    return input.replace(/[^a-z0-9 -]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-');
+  }
+
+};
+
+// Export helpers
+module.exports.register = function (Handlebars, options) {
+  options = options || {};
+
+  for (var helper in helpers) {
+    if (helpers.hasOwnProperty(helper)) {
+      Handlebars.registerHelper(helper, helpers[helper]);
+    }
+  }
+};
+
+
